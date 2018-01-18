553625d43dd28ce04c6f2f31daa08ece4b373d3e
Lab Computer <varun51194@gmail.com>
2018-01-18 11:36:58 -0800

    lab3 gripper swaggery

--- fetch_api/src/fetch_api/gripper.py
+++ fetch_api/src/fetch_api/gripper.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 
-# TODO: import ?????????
-# TODO: import ???????_msgs.msg
+import actionlib
+from control_msgs.msg import GripperCommandAction, GripperCommandGoal
 import rospy
 
-# TODO: ACTION_NAME = ???
+ACTION_NAME = 'gripper_controller/gripper_action'
 CLOSED_POS = 0.0  # The position for a fully-closed gripper (meters).
 OPENED_POS = 0.10  # The position for a fully-open gripper (meters).
 
@@ -16,17 +16,15 @@ class Gripper(object):
     MAX_EFFORT = 100  # Max grasp force, in Newtons
 
     def __init__(self):
-        # TODO: Create actionlib client
-        # TODO: Wait for server
-        pass
+        #  Create actionlib self._client
+        self._client = actionlib.SimpleActionClient(ACTION_NAME, GripperCommandAction)
+        #  Wait for server
+        self._client.wait_for_server()
 
     def open(self):
         """Opens the gripper.
         """
-        # TODO: Create goal
-        # TODO: Send goal
-        # TODO: Wait for result
-        rospy.logerr('Not implemented.')
+        self.gripper_movement(OPENED_POS)
 
     def close(self, max_effort=MAX_EFFORT):
         """Closes the gripper.
@@ -35,7 +33,14 @@ class Gripper(object):
             max_effort: The maximum effort, in Newtons, to use. Note that this
                 should not be less than 35N, or else the gripper may not close.
         """
-        # TODO: Create goal
-        # TODO: Send goal
-        # TODO: Wait for result
-        rospy.logerr('Not implemented.')
+        self.gripper_movement(CLOSED_POS, max_effort)
+
+    def gripper_movement(self, position, max_effort=MAX_EFFORT):
+        #  Create goal
+        goal = GripperCommandGoal()
+        goal.command.position = position
+        goal.command.max_effort = max_effort
+        #  Send goal
+        self._client.send_goal(goal)
+        #  Wait for result
+        self._client.wait_for_result(rospy.Duration.from_sec(5.0))
