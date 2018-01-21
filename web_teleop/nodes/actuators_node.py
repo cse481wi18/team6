9a111a823c54e4d0cc3a6db5952b8a74830a02c4
Lab Computer <varun51194@gmail.com>
2018-01-20 16:15:40 -0800

    Add arm movement and gripping

--- web_teleop/nodes/actuators_node.py
+++ web_teleop/nodes/actuators_node.py
@@ -2,7 +2,10 @@
 
 import fetch_api
 import rospy
-from web_teleop.srv import SetTorso, SetTorsoResponse, MoveHead, MoveHeadResponse
+from web_teleop.srv import SetTorso, SetTorsoResponse,\
+        MoveHead, MoveHeadResponse,\
+        MoveArm, MoveArmResponse,\
+        SetGrip, SetGripResponse
 from joint_state_reader import JointStateReader
 
 
@@ -17,6 +20,8 @@ class ActuatorServer(object):
     def __init__(self):
         self._torso = fetch_api.Torso()
         self._head = fetch_api.Head()
+        self._grip = fetch_api.Gripper()
+        self._arm = fetch_api.Arm()
         self._reader = JointStateReader()
 
     def handle_set_torso(self, request):
@@ -26,13 +31,26 @@ class ActuatorServer(object):
     def handle_move_head(self, move_head_request):
         if (move_head_request.focus_hand):
             self._head.look_at('wrist_flex_link', 0, 0, 0)
-            return MoveHeadResponse()
         else:
             curr_pan = self._reader.get_joint('head_pan_joint')
             curr_tilt = self._reader.get_joint('head_tilt_joint')
             self._head.pan_tilt(curr_pan + move_head_request.delta_pan,
                     curr_tilt + move_head_request.delta_tilt)
-            return MoveHeadResponse()
+        return MoveHeadResponse()
+
+    def set_grip(self, set_grip_request):
+        if not set_grip_request.grip:
+            self._grip.open()
+        else:
+            self._grip.close(set_grip_request.effort)
+        return SetGripResponse()
+
+
+    def handle_move_arm(self, move_arm_request):
+        rospy.loginfo(move_arm_request)
+        self._arm.move_to_joints(fetch_api.ArmJoints.from_list(move_arm_request.positions))
+        return MoveArmResponse()
+
 
 def main():
     rospy.init_node('web_teleop_actuators')
@@ -42,6 +60,10 @@ def main():
                                   server.handle_set_torso)
     head_service = rospy.Service('web_teleop/move_head', MoveHead,
                                   server.handle_move_head)
+    arm_service = rospy.Service('web_teleop/move_arm', MoveArm,
+                                 server.handle_move_arm)
+    gripper_service = rospy.Service('web_teleop/set_grip', SetGrip,
+                                     server.set_grip)
     rospy.spin()
 
 
