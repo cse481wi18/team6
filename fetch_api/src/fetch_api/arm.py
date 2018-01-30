0ae67c27a2abfab7ba6fc0363ce7cd6b3dbd3648
Lab Computer <varun51194@gmail.com>
2018-01-30 11:51:02 -0800

    Lab 19 done

--- fetch_api/src/fetch_api/arm.py
+++ fetch_api/src/fetch_api/arm.py
@@ -2,10 +2,13 @@ import actionlib
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 import rospy
+from .moveit_goal_builder import MoveItGoalBuilder
+from moveit_msgs.msg import MoveItErrorCodes, MoveGroupAction
 
 from .arm_joints import ArmJoints
 
 ACTION_NAME = 'arm_controller/follow_joint_trajectory'
+MOVEIT_ACTION_NAME = 'move_group'
 
 TIME_FROM_START = 5
 
@@ -20,9 +23,12 @@ class Arm(object):
 
     def __init__(self):
         # TODO: Create actionlib client
-        self._client = actionlib.SimpleActionClient(ACTION_NAME, FollowJointTrajectoryAction)
+        self._joint_tr_client = actionlib.SimpleActionClient(ACTION_NAME, FollowJointTrajectoryAction)
+        # Make MoveItGoal client
+        self._moveit_client = actionlib.SimpleActionClient(MOVEIT_ACTION_NAME, MoveGroupAction)
         # TODO: Wait for server
-        self._client.wait_for_server()
+        self._joint_tr_client.wait_for_server()
+        self._moveit_client.wait_for_server()
 
     def move_to_joints(self, arm_joints):
         """Moves the robot's arm to the given joints.
@@ -43,6 +49,91 @@ class Arm(object):
         # TODO: Add the trajectory point created above to trajectory
         goal.trajectory.points.append(point)
         # TODO: Send goal
-        self._client.send_goal(goal)
+        self._joint_tr_client.send_goal(goal)
         # TODO: Wait for result
-        self._client.wait_for_result(rospy.Duration.from_sec(TIME_FROM_START))
+        self._joint_tr_client.wait_for_result(rospy.Duration.from_sec(TIME_FROM_START))
+
+    def move_to_pose(self, pose_stamped):
+        """Moves the end-effector to a pose, using motion planning.
+
+        Args:
+            pose: geometry_msgs/PoseStamped. The goal pose for the gripper.
+
+        Returns:
+            string describing the error if an error occurred, else None.
+        """
+        goal_builder = MoveItGoalBuilder()
+        goal_builder.set_pose_goal(pose_stamped)
+        goal = goal_builder.build()
+        self._moveit_client.send_goal(goal)
+        self._moveit_client.wait_for_result()
+        result = self._moveit_client.get_result()
+        if result.error_code == MoveItErrorCodes.SUCCESS:
+            return None
+        else:
+            return Arm.moveit_error_string(result.error_code.val)
+    
+    @staticmethod   
+    def moveit_error_string(val):
+        """Returns a string associated with a MoveItErrorCode.
+            
+        Args:
+            val: The val field from moveit_msgs/MoveItErrorCodes.msg
+            
+        Returns: The string associated with the error value, 'UNKNOWN_ERROR_CODE'
+            if the value is invalid.
+        """ 
+        if val == MoveItErrorCodes.SUCCESS:
+            return 'SUCCESS'
+        elif val == MoveItErrorCodes.FAILURE:
+            return 'FAILURE'
+        elif val == MoveItErrorCodes.PLANNING_FAILED:
+            return 'PLANNING_FAILED'
+        elif val == MoveItErrorCodes.INVALID_MOTION_PLAN:
+            return 'INVALID_MOTION_PLAN'
+        elif val == MoveItErrorCodes.MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE:
+            return 'MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE'
+        elif val == MoveItErrorCodes.CONTROL_FAILED:
+            return 'CONTROL_FAILED'
+        elif val == MoveItErrorCodes.UNABLE_TO_AQUIRE_SENSOR_DATA:
+            return 'UNABLE_TO_AQUIRE_SENSOR_DATA'
+        elif val == MoveItErrorCodes.TIMED_OUT:
+            return 'TIMED_OUT'
+        elif val == MoveItErrorCodes.PREEMPTED:
+            return 'PREEMPTED'
+        elif val == MoveItErrorCodes.START_STATE_IN_COLLISION:
+            return 'START_STATE_IN_COLLISION'
+        elif val == MoveItErrorCodes.START_STATE_VIOLATES_PATH_CONSTRAINTS:
+            return 'START_STATE_VIOLATES_PATH_CONSTRAINTS'
+        elif val == MoveItErrorCodes.GOAL_IN_COLLISION:
+            return 'GOAL_IN_COLLISION'
+        elif val == MoveItErrorCodes.GOAL_VIOLATES_PATH_CONSTRAINTS:
+            return 'GOAL_VIOLATES_PATH_CONSTRAINTS'
+        elif val == MoveItErrorCodes.GOAL_CONSTRAINTS_VIOLATED:
+            return 'GOAL_CONSTRAINTS_VIOLATED'
+        elif val == MoveItErrorCodes.INVALID_GROUP_NAME:
+            return 'INVALID_GROUP_NAME'
+        elif val == MoveItErrorCodes.INVALID_GOAL_CONSTRAINTS:
+            return 'INVALID_GOAL_CONSTRAINTS'
+        elif val == MoveItErrorCodes.INVALID_ROBOT_STATE:
+            return 'INVALID_ROBOT_STATE'
+        elif val == MoveItErrorCodes.INVALID_LINK_NAME:
+            return 'INVALID_LINK_NAME'                                      
+        elif val == MoveItErrorCodes.INVALID_OBJECT_NAME:
+            return 'INVALID_OBJECT_NAME'
+        elif val == MoveItErrorCodes.FRAME_TRANSFORM_FAILURE:
+            return 'FRAME_TRANSFORM_FAILURE'
+        elif val == MoveItErrorCodes.COLLISION_CHECKING_UNAVAILABLE:
+            return 'COLLISION_CHECKING_UNAVAILABLE'
+        elif val == MoveItErrorCodes.ROBOT_STATE_STALE:
+            return 'ROBOT_STATE_STALE'
+        elif val == MoveItErrorCodes.SENSOR_INFO_STALE:
+            return 'SENSOR_INFO_STALE'
+        elif val == MoveItErrorCodes.NO_IK_SOLUTION:
+            return 'NO_IK_SOLUTION'
+        else:
+            return 'UNKNOWN_ERROR_CODE: ' + str(val)
+
+    def cancel_all_goals(self):
+        self._joint_tr_client.cancel_all_goals() # Your action client from Lab 7
+        self._moveit_client.cancel_all_goals() # From this lab
