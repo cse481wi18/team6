9a0b2fd631dd3935d7ecb70892af6cd48d64ad38
Lab Computer <varun51194@gmail.com>
2018-02-28 21:45:30 -0800

    Make 3 topics for each type

--- recycle-ui/nodes/classification_server.py
+++ recycle-ui/nodes/classification_server.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import PointCloud2
 from perception import MockCamera
 import sqlite3
 
+WASTE_TYPES = ['trash', 'recycle', 'compost']
+
 def wait_for_time():
     """Wait for simulated time to begin.
     """
@@ -15,12 +17,13 @@ def wait_for_time():
 DB_FILE = '/home/team6/catkin_ws/src/cse481wi18/database/recycle.db'
 class ClassificationServer():
 
-    def __init__(self):
+    def __init__(self, waste_type):
+        self._waste_type = waste_type
         rospy.logerr("CONSTRUCTING SERVER")
-        self._log_page_sub = rospy.Subscriber('/recycle_ui/get_log_page',
+        self._log_page_sub = rospy.Subscriber('/recycle_ui/get_{}_log_page'.format(waste_type),
                 GetLogPage,
                 callback=self._handle_get_log_page)
-        self._active_log_pub = rospy.Publisher('/recycle_ui/active_logs',
+        self._active_log_pub = rospy.Publisher('/recycle_ui/active_{}_logs'.format(waste_type),
                 ActiveLogs,
                 latch=True,
                 queue_size=10)
@@ -32,6 +35,7 @@ class ClassificationServer():
         conn = sqlite3.connect(DB_FILE)
         rospy.logerr('DB connected')
         c = conn.cursor()
+        # TODO Run this query once for each waste_type in waste_types
         c.execute('''SELECT
                 cl.log_id,
                 pred_ol.label,
@@ -44,24 +48,26 @@ class ClassificationServer():
             WHERE pred_cat.name=?
             AND cl.log_id > ?
             LIMIT ?''',
-            (get_log_page.waste_type, get_log_page.starting_log_num, get_log_page.page_size))
+            (self._waste_type, get_log_page.starting_log_num, get_log_page.page_size))
         logs = c.fetchall()
 
         # Hunter is doing this for testing because He fucked up db
         test_data = {
-            'recycle': [(1, 'paper', 'unknown', 'not/sure'), (4, 'coffee', 'unknown', 'not/sure')],
-            'trash': [(2, 'wrapper', 'unknown', 'not/sure')],
-            'compost': [(3, 'apple', 'unknown', 'not/sure')]
+            'recycle': [(1, 'paper', 'unknown', 'recycle', 'not/sure'), (4, 'coffee', 'unknown', 'recycle', 'not/sure')],
+            'trash': [(2, 'wrapper', 'unknown', 'trash', 'not/sure')],
+            'compost': [(3, 'apple', 'unknown', 'compost', 'not/sure')]
         };
-        logs = test_data[get_log_page.waste_type]
+
+        logs = test_data[self._waste_type]
         log_items = []
         for log in logs:
             # build response fields
             rospy.logerr(log)
             rospy.logerr("PROCESSING LOG")
             cloud_topic_name = 'log{}_pointcloud'.format(log[0])
-            log_items.append(LogItem(predicted_item_label=str(log[1]),
-                predicted_waste_type=get_log_page.waste_type,
+            log_items.append(LogItem(log_id=log[0],
+                predicted_item_label=str(log[1]),
+                predicted_waste_type=self._waste_type,
                 actual_label=str(log[2]),
                 pointcloud_topic=cloud_topic_name))
 
@@ -79,7 +85,7 @@ class ClassificationServer():
 def main():
     rospy.init_node('recycle-ui-classification')
     wait_for_time()
-    server = ClassificationServer()
+    servers = [ClassificationServer(waste_type) for waste_type in WASTE_TYPES]
     rospy.spin()
 
 if __name__ == '__main__':
