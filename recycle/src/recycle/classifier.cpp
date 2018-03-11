0ce20d3348630b2c675ba30d369113654b6495b2
Lab Computer <varun51194@gmail.com>
2018-03-10 17:21:20 -0800

    transforming point cloud frame

--- recycle/src/recycle/classifier.cpp
+++ recycle/src/recycle/classifier.cpp
@@ -27,8 +27,26 @@ namespace recycle {
     
     sensor_msgs::PointCloud2ConstPtr msg = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("cloud_in");
     
+    tf::TransformListener tf_listener;                                                    
+    tf_listener.waitForTransform("base_link", msg->header.frame_id,                     
+                                 ros::Time(0), ros::Duration(5.0));                       
+    tf::StampedTransform transform;                                                       
+    try {                                                                                 
+      tf_listener.lookupTransform("base_link", msg->header.frame_id,                    
+                                  ros::Time(0), transform);                               
+    } catch (tf::LookupException& e) {                                                    
+      std::cerr << e.what() << std::endl;                                                 
+      // return 1;                                                                           
+    } catch (tf::ExtrapolationException& e) {                                             
+      std::cerr << e.what() << std::endl;                                                 
+      // return 1;                                                                           
+    }                                                                                     
+                                                                                            
+    sensor_msgs::PointCloud2 cloud_out;                                                   
+    pcl_ros::transformPointCloud("base_link", transform, *msg, cloud_out);
+
     PointCloudC::Ptr cloud(new PointCloudC());
-    pcl::fromROSMsg(* msg, *cloud);
+    pcl::fromROSMsg(cloud_out, *cloud);
 
     Cropper cropper(crop_pub_);
     PointCloudC::Ptr cropped = cropper.Crop(cloud);
